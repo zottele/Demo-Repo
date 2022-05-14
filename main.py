b431f3135444c9fb8e4aa2e65938cea6fcb22d03
Nektar Volanaki <nevo47111@gmail.com>
2022-05-14 22:30:50 +0200

    Mach em Debugtest

--- main.py
+++ main.py
@@ -6,15 +6,21 @@ import math
 import os
 import sys
 
+import requests
+
+
 import numpy as np
 
-print(sys.version)
-print(sys.executable)
+# print(sys.version)
+# print(sys.executable)
 
 
 def main():
+
     test = "Test"
+    r = requests.get("https://coreyms.com")
+    print(r.status_code)
 
 
-if __name__ == "main":
+if __name__ == "__main__":
     main()
