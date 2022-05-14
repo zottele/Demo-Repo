2710d8177ee66e32f562f1212986f87e8e5d86b6
Nektar Volanaki <nevo47111@gmail.com>
2022-05-14 22:04:02 +0200

    einn wenig gespielt

--- main.py
+++ main.py
@@ -2,11 +2,19 @@
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 #
+import math
+import os
+import sys
+
+import numpy as np
+
+print(sys.version)
+print(sys.executable)
 
 
 def main():
-    pass
+    test = "Test"
 
 
-if __name__ == 'main':
+if __name__ == "main":
     main()
